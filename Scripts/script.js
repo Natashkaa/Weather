bf344f27e32109df55fe074c1a3d064ea71d805a
Enot <natasha.cat982404@gmail.com>
2020-02-12 21:20:25 +0200

    Get weather(current day) by coords and city

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,59 +1,50 @@
-/*classes*/
-let url = "https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?";
-let city = "Kiev";
-let apiKey = "a49244ade877ce6d9959e8846248bb70";
-var urlByCoords;
-var urlByCity;
-
-$('#current_date').text(`${new Date().getDate()}.${new Date().getMonth() + 1}.${new Date().getFullYear()}`);
-if ("geolocation" in navigator) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-        urlByCoords = `${url}lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}`;
-        FEETCH(urlByCoords).then(x => {
-            $('#weather_main').text(x.weather[0].main);
-            $('#current_weather_img').attr("src", `../Source/${x.weather[0].icon}.png`);
-            $('#current_temperature').text(`${x.main.temp-273,15}°C`);
-            $('#current_pressure').text(`Pressure: ${(x.main.pressure/1.333).toFixed(0)}`);
-            let unix_timestampSunrise = x.sys.sunrise;
-            let dateSunrise = new Date(unix_timestampSunrise * 1000);
-            let hours1 = dateSunrise.getHours();
-            let minutes1 = dateSunrise.getMinutes();
-            var formattedTimeSunrise = hours1 + ':' + minutes1;
-            
-            let unix_timestampSunset = x.sys.sunset;
-            let dateSunset = new Date(unix_timestampSunset * 1000);
-            let hours2 = dateSunset.getHours();
-            let minutes2 = dateSunset.getMinutes();
-            var formattedTimeSunset= hours2 + ':' + minutes2;
-            $('#sunrise').text(`Sunrise: ${formattedTimeSunrise}`);
-            $('#sunset').text(`Sunrise: ${formattedTimeSunset}`);
-            let duration;
-            if(minutes1 < minutes2){
-                duration = `${hours2 - hours1}h ${minutes2 - minutes1}m`;
-                $('#duration').text(`Duration: ${duration}`);
-            }
-            else{
-                duration = `${(hours2-1) - hours1}h ${60-(minutes1 - minutes2)}m`;
-                $('#duration').text(`Duration: ${duration}`);
-            }
-            
+window.onload = function() {
+    $('#btn_today').addClass('active-btn');
+    $('#current_date').text(`${new Date().getDate()}.${new Date().getMonth() + 1}.${new Date().getFullYear()}`);
+    if ("geolocation" in navigator)/*search by coords*/  {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            FEETCH(data.getUrlTodayCoords(position.coords.latitude, position.coords.longitude)).then(x => {
+                SetData(x);
+            });
         });
-        // urlByCity = `${url}q=${city}&appid=${apiKey}`;
-        // FEETCH(urlByCoords).then(x => {
-        // alert(x.status);
-        // });
-    });
-} else {
-    
-}
-
-
+    } else {//search by city
+        urlByCity = `${url}q=${city}&appid=${apiKey}`;
+        FEETCH(data.getUrlTodayCity(data.defaultCity)).then(x => {
+            SetData(x);
+        });
+    }
+};
 
 $('#btn_find').on( "click", function() {
-    urlByCity = `${url}q=${city}&appid=${apiKey}`;
-    FEETCH(urlByCoords).then(x => {
-        alert(x.status);
-    });
+    let userCity = $('#inp_city').val();
+        FEETCH(data.getUrlTodayCity(userCity)).then(x => {
+            if(x.cod != 200){
+                $('#error_row').css({"display":"block"});
+                $('.error-descr').text(`${userCity} could not be found. Please try enter a different location.`);
+                $('.show-weather').css({"display":"none"});
+            }
+            else{
+            $('#error_row').css({"display":"none"});
+            $('.show-weather').css({"display":"block"});
+
+            SetData(x);
+            }
+        });
+});
+$('#btn_5day').on( "click", function() {
+    $('#btn_today').removeClass('active-btn');
+    $(this).addClass('active-btn');
+    if($('#error_row').css('display') == "none"){
+        $('.show-weather').css({"display":"none"})
+    }
+    
+});
+$('#btn_today').on( "click", function() {
+    $('#btn_5day').removeClass('active-btn');
+    $(this).addClass('active-btn');
+    if($('#error_row').css('display') == "none"){
+        $('.show-weather').css({"display":"block"})
+    }
 });
 
 
